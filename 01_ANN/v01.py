56272e438bb0b09ee662de9e77a18ed6470c43aa
JuanMHuerta <jmhuertany@gmail.com>
2019-03-07 14:00:57 -0500

    Cosmetic changes

--- 01_ANN/v01.py
+++ 01_ANN/v01.py
@@ -1,52 +1,15 @@
-
 import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
-
 # Based on example from https://towardsdatascience.com/neural-networks-from-scratch-easy-vs-hard-b26ddc2e89c7
-
-
-# STEP 1: Load data, get onehot encoding of targets and split into training and testing
-dig = load_digits()
-onehot_target = [[1 if y==x else 0 for y in range(10)] for x in dig.target]
-x_train, x_val, y_train, y_val = train_test_split(dig.data, onehot_target, test_size=0.1, random_state=20)
-
-
-# STEP 2: Utility functions
-
-def sigmoid(s):
-    return 1/(1 + np.exp(-s))
-
-def sigmoid_derv(s):
-    return s * (1 - s)
-
-def softmax(s):
-    exps = np.exp(s - np.max(s, axis=1, keepdims=True))
-    return exps/np.sum(exps, axis=1, keepdims=True)
-
-def cross_entropy(pred, real):
-    n_samples = real.shape[0]
-    res = pred - real
-    return res/n_samples
-
-def error(pred, real):
-    n_samples = real.shape[0]
-    logp = - np.log(pred[np.arange(n_samples), real.argmax(axis=1)])
-    loss = np.sum(logp)/n_samples
-    return loss
-
-
-
-## STEP 3: NN Class:
 
 class MyNN:
     def __init__(self, x, y):
         self.x = x
         neurons = 128
-        self.lr = 0.5
+        self.lr = 0.50
         ip_dim = x.shape[1]
         op_dim = y.shape[1]
-
         self.w1 = np.random.randn(ip_dim, neurons)
         self.b1 = np.zeros((1, neurons))
         self.w2 = np.random.randn(neurons, neurons)
@@ -55,22 +18,53 @@ class MyNN:
         self.b3 = np.zeros((1, op_dim))
         self.y = y
 
+    def fit(self,**kwargs):
+        if 'epochs' in kwargs:
+            epochs=kwargs['epochs']
+        else:
+            epochs = 1500
+        for x in range(epochs):
+            print("Epoch:",x, end=' ')
+            self.feedforward()
+            self.backprop()
+
+    def sigmoid(self,s):
+        return 1/(1 + np.exp(-s))
+
+    def sigmoid_derv(self,s):
+        return s * (1 - s)
+
+    def softmax(self,s):
+        exps = np.exp(s - np.max(s, axis=1, keepdims=True))
+        return exps/np.sum(exps, axis=1, keepdims=True)
+
+    def cross_entropy(self,pred, real):
+        n_samples = real.shape[0]
+        res = pred - real
+        return res/n_samples
+
+    def error(self,pred, real):
+        n_samples = real.shape[0]
+        logp = - np.log(pred[np.arange(n_samples), real.argmax(axis=1)])
+        loss = np.sum(logp)/n_samples
+        return loss
+
     def feedforward(self):
         z1 = np.dot(self.x, self.w1) + self.b1
-        self.a1 = sigmoid(z1)
+        self.a1 = self.sigmoid(z1)
         z2 = np.dot(self.a1, self.w2) + self.b2
-        self.a2 = sigmoid(z2)
+        self.a2 = self.sigmoid(z2)
         z3 = np.dot(self.a2, self.w3) + self.b3
-        self.a3 = sigmoid(z3)
+        self.a3 = self.sigmoid(z3)
         
     def backprop(self):
-        loss = error(self.a3, self.y)
+        loss = self.error(self.a3, self.y)
         print('Error :', loss)
-        a3_delta = cross_entropy(self.a3, self.y) # w3
+        a3_delta = self.cross_entropy(self.a3, self.y) # w3
         z2_delta = np.dot(a3_delta, self.w3.T)
-        a2_delta = z2_delta * sigmoid_derv(self.a2) # w2
+        a2_delta = z2_delta * self.sigmoid_derv(self.a2) # w2
         z1_delta = np.dot(a2_delta, self.w2.T)
-        a1_delta = z1_delta * sigmoid_derv(self.a1) # w1
+        a1_delta = z1_delta * self.sigmoid_derv(self.a1) # w1
 
         self.w3 -= self.lr * np.dot(self.a2.T, a3_delta)
         self.b3 -= self.lr * np.sum(a3_delta, axis=0, keepdims=True)
@@ -84,15 +78,14 @@ class MyNN:
         self.feedforward()
         return self.a3.argmax()
     
-## STEP 4: Classification Exercise
+# STEP 1: Load data, produce one-hot encoding of targets and split into training and testing
+dig = load_digits()
+onehot_target = [[1 if y==x else 0 for y in range(10)] for x in dig.target]
+x_train, x_val, y_train, y_val = train_test_split(dig.data, onehot_target, test_size=0.1, random_state=20)
 
+## STEP 2: Create network, fit the data
 model = MyNN(x_train/16.0, np.array(y_train))
-epochs = 1500
-
-for x in range(epochs):
-    print("Epoch:",x, end=' ')
-    model.feedforward()
-    model.backprop()
+model.fit(epochs=1500)
 
 def get_acc(x, y):
     acc = 0
@@ -102,8 +95,6 @@ def get_acc(x, y):
             acc +=1
     return acc/len(x)*100
 
-print("Training accuracy : ", get_acc(x_train/16, np.array(y_train)))
-print("Test accuracy : ", get_acc(x_val/16, np.array(y_val)))
-
-
-
+## STEP 3: Bencmkark Accuracy
+print("Training accuracy : ", get_acc(x_train/16.0, np.array(y_train)))
+print("Test accuracy : ", get_acc(x_val/16.0, np.array(y_val)))
